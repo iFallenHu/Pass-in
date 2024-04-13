5d5563ab67cf3877e9052119bde883284b612981
MCPLUS-LUAN <luannascimento.microcamp@gmail.com>
2024-04-12 21:05:32 -0300

    Removido o UUID da classe checkin

--- src/main/java/com/techsolucoes/passin/domain/checkin/CheckIn.java
+++ src/main/java/com/techsolucoes/passin/domain/checkin/CheckIn.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 public class CheckIn {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     @Column(name = "created_at")
